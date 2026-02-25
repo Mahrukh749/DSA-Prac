bb03b5e08a8a5dbea20f6dbaa76c0c25010cbc1e
Mahrukh749 <mahrukhmahrukh111@gmail.com>
2026-02-25 08:43:59 -0800

    Add files via upload

--- prac.cpp
+++ prac.cpp
@@ -1,20 +1,30 @@
+//
 #include <iostream>
+#include <climits>
 using namespace std;
+
 int main()
 {
+    int arr[] = {5, 1, 9, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
-    int arr[5] = {3, 5, 7, 9, 11};
-    int n = 5;
-
-    int max = arr[0];
+    int smallest = INT_MAX;
+    int secondSmallest = INT_MAX;
 
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] > max)
+        if (arr[i] < smallest)
         {
-            max = arr[i];
+            secondSmallest = smallest;
+            smallest = arr[i];
+        }
+        else if (arr[i] > smallest && arr[i] < secondSmallest)
+        {
+            secondSmallest = arr[i];
         }
     }
-    cout << "Maximum element is: " << max << endl;
+
+    cout << "Second Smallest Element: " << secondSmallest;
+
     return 0;
-};
+}
